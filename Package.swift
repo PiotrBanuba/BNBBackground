c42263326c3b845bcdc3f5df77d2d0a8b71c4b8a
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:51:59 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBBackground",
@@ -13,9 +13,9 @@ let package = Package(
             name: "BNBBackground",
             targets: [
                 "BNBBackground",
-                "BNBBackgroundBNBSdkCore",
-                "BNBBackgroundBNBEffectPlayer",
-                "BNBBackgroundBNBScripting"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting"
             ]
         ),
     ],
@@ -38,33 +38,6 @@ let package = Package(
             name: "BNBBackground",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBBackground.zip",
             checksum: "88a07cca081eba1d1bff8263030f4566ea3ad50cdf808f5149a28053f2ace91c"
-        ),
-        .target(
-            name: "BNBBackgroundBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBBackgroundBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBBackgroundBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
         ),
     ]
 )
