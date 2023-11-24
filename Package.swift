17346541219ddcbbbaa1754a954f5488cd26f1a5
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-11-24 11:49:35 +0300

    1.9.1-83-gf89cbbd807

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-64-g951557ebd1"
+let bnbPackageVersion: Version = "1.9.1-83-gf89cbbd807"
 
 let package = Package(
     name: "BNBBackground",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBBackground",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-64-g951557ebd1/BNBBackground.zip",
-            checksum: "d78319c9194baec5d7bf07ce0f65a720247600c5f6627ef771124679869459b7"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-83-gf89cbbd807/BNBBackground.zip",
+            checksum: "d9216de7ad3745180bd38528cb27d8ee254d6a374e87a877a49fb759e1664206"
         ),
         .target(
             name: "BNBBackground_BNBSdkCore",
